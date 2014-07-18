8eb2066aead920e85ad6535f5c8ed9d05d168f38
Amy Rudolph <amy.rudolph@live.co.uk>
2014-07-19 00:37:03 +0400

    fixed bug in pin.rb file

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,6 +4,6 @@ class Pin < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
-	alidates :image, presence: true
+	validates :image, presence: true
   validates :description, presence: true
 end
